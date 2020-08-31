c1c3d1a567456399075af63993d2cb922bbd5f69
Ian Harold <ianhunterharold@gmail.com>
2020-08-31 17:26:28 -0600

    re-orged and gutted react app, added basic container and component folders, added list container file

---
+++ src/containers/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react';
+import '../style/List.css';
+
+
+class List extends Component {
+    render(){
+        return(
+            <div className="body">
+                list!
+            </div>
+        )
+    }
+}
+
+export default List;
