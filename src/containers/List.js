a7ec8cc707851656bf15d88d7eb73dc3b0e0977f
Ian Harold <ianhunterharold@gmail.com>
2020-09-01 16:43:28 -0600

    updating state with user input to form

--- src/containers/List.js
+++ src/containers/List.js
@@ -4,10 +4,31 @@ import '../style/List.css';
 
 
 class List extends Component {
+
+    state = {
+        todoInput: ''
+    }
+
+    handleChange =(event)=> {
+        this.setState({
+            todoInput: event.target.value
+        })
+    }
+
+
+    
+
     render(){
         return(
             <div className="body">
-                list!
+                Your Daily Tasks
+                <form>
+                    <label>
+                        
+                        <input type="text" name="todo" placeholder="never ending list" value={this.state.todoInput} onChange={this.handleChange} />
+                    </label>
+                        <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
