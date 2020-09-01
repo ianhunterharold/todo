0248ff6df44b22874556b84a2c95392d0bf5f061
Ian Harold <ianhunterharold@gmail.com>
2020-09-01 17:28:53 -0600

    state change on the front end and array is updating with new input:

--- src/containers/List.js
+++ src/containers/List.js
@@ -6,7 +6,8 @@ import '../style/List.css';
 class List extends Component {
 
     state = {
-        todoInput: ''
+        todoInput: '',
+        todoList: ["buy groceries", "shop for new shoes","buy tv"],
     }
 
     handleChange =(event)=> {
@@ -15,21 +16,47 @@ class List extends Component {
         })
     }
 
+    mapOverToDoItems = () => {
+        return this.state.todoList.map((listItems)=> {
+            return (
+                <div>
+                    <ul>
+                        {listItems}
+                    </ul>
+                </div>
+            )
+        })
+    }
 
+    handleToDoSubmit = (e) => {
+        e.preventDefault();
+        console.log("you will send this to your backend to persist that data then you will bring it back and repost it on your page for now we will add it to the state of the array")
+        this.setState( (previousState) => {
+            return {
+                todoList: [...previousState.todoList,this.state.todoInput]
+            }
+        })
+
+    }
     
 
     render(){
         return(
-            <div className="body">
-                Your Daily Tasks
-                <form>
-                    <label>
-                        
-                        <input type="text" name="todo" placeholder="never ending list" value={this.state.todoInput} onChange={this.handleChange} />
-                    </label>
-                        <input type="submit" value="Submit" />
-                </form>
-            </div>
+            <>
+                <div className="body">
+                    Your Daily Tasks
+                    <form onSubmit={this.handleToDoSubmit}>
+                        <label>
+                            
+                            <input type="text" name="todo" placeholder="never ending list" value={this.state.todoInput} onChange={this.handleChange} />
+                        </label>
+                            <input type="submit" value="Submit" />
+                    </form>
+                    <div className="list">
+                    {this.mapOverToDoItems()}  
+                    </div>
+                </div>
+            </>
         )
     }
 }
